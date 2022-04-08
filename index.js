488d26ec79a6616487a653c56e6d03da9c201cb0
Brian Sitati <briansit93@gmail.com>
2022-04-08 17:14:20 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+
+/*An arrow function with square*/
+const square = (a ) => 2 * 2;
+/*An arrow function with divide*/
+const divide = (a, b) => 2000 / 100;
+
+/*An arrow function with adde*/
+const add = (a, b) => 5 + 2;
